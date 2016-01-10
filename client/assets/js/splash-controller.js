6cb10392cf0506c869ecfc1bd5feac1998ec9b51
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-01-10 01:01:52 -0500

    make login button functional

--- client/assets/js/splash-controller.js
+++ client/assets/js/splash-controller.js
@@ -1,3 +1,12 @@
-angular.module('portal').controller('splashController', function ($scope) {
-    
+angular.module('portal').controller('splashController', function ($scope, $http, $location) {
+    $scope.submitLogin = function () {
+        $http.post('/api/user/login', {
+            'email': $scope.email,
+            'password': $scope.password
+        }).then(function (response) {
+            $location.path('/home');
+        }, function (response) {
+            swal('Oops...', response.data, 'error');
+        });
+    }
 });
