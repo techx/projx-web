5a11a6060306b31c11b6982e88c2ef55e308dfd1
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-01-10 16:52:33 -0500

    home profile

--- client/assets/js/home-controller.js
+++ client/assets/js/home-controller.js
@@ -1,5 +1,17 @@
 angular.module('portal').controller('homeController', function ($scope, $http) {
 
+    // page title
     $scope.title = 'home';
+
+    // profile section
+    $scope.profileFields = ['email', 'name', 'phone'];
+    $scope.editProfile = function () {
+        // todo: edit profile
+    }
+
+    // projects section
+    $http.get('/api/project/current').then(function (response) {
+        $scope.projects = response.data;
+    });
 
 });
