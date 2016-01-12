c9adbc255cb433c909c075158ee5cf8275d44c91
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-01-12 05:15:42 -0500

    no profile in home

--- client/assets/js/home-controller.js
+++ client/assets/js/home-controller.js
@@ -3,20 +3,6 @@ angular.module('portal').controller('homeController', function ($scope, $http, $
     // page title
     $scope.title = 'home';
 
-    // specify which fields to display (maps field name to key in project object)
-    $scope.profileFields = {
-        'email': 'email',
-        'name': 'name',
-        'phone': 'phone',
-        'course': 'course',
-        'year': 'year'
-    };
-
-    // edit profile
-    $scope.editProfile = function () {
-        swal('Profile editing coming soon...');
-    }
-
     // projects section
     $http.get('/api/project/current').then(function (response) {
         $scope.projects = response.data;
