68fab29a35ca6427171724784a5606952f1e9b3e
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-01-12 23:45:35 -0500

    oops... remove config (and change secrets)

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'mongoUri': process.env.MONGOLAB_URI || 'mongodb://localhost/projxportal',
-    'cookieSecret': process.env.COOKIE_SECRET || 'notasecret',
-    'authSecret': '6EqJDKkT3kEB8CFB883x',
-    'authUrl': 'https://vfazel.scripts.mit.edu:444/projx-auth.php'
-}
