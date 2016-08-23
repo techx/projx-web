6a0ab2381263c918e59d08dfff92e2cd6221724c
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-08-22 23:51:32 -0400

    write forum view

--- client/app/views/forum/forum.js
+++ client/app/views/forum/forum.js
@@ -1,1 +1,43 @@
-angular.module('projx').controller('forumController', function () {});
+angular.module('projx').controller('forumController', function ($scope) {
+    var fakeThreads = [{
+        'id': '1',
+        'author': 'vfazel',
+        'date': 'Jun 8, 2016',
+        'title': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.',
+        'content': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.'
+    }, {
+        'id': '12',
+        'author': 'vfazel',
+        'date': 'Jun 8, 2016',
+        'title': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.',
+        'content': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.'
+    }, {
+        'id': '123',
+        'author': 'vfazel',
+        'date': 'Jun 8, 2016',
+        'title': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.',
+        'content': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.'
+    }, {
+        'id': '1234',
+        'author': 'vfazel',
+        'date': 'Jun 8, 2016',
+        'title': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.',
+        'content': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.'
+    }, {
+        'id': '12345',
+        'author': 'vfazel',
+        'date': 'Jun 8, 2016',
+        'title': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.',
+        'content': 'Why can\'t I get my header to fit in this card? I want to write a very long header here.'
+    }];
+
+    $scope.threads = fakeThreads;
+
+    $scope.truncate = function (text, cutoff) {
+        if (text.length > cutoff) {
+            return text.substring(0, cutoff) + '...';
+        } else {
+            return text;
+        }
+    }
+});
