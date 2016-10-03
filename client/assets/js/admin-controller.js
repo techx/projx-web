4af9f770ad82665ccbf836477a631e97cd01eeda
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-10-03 13:11:03 -0400

    add more info on admin page

--- client/assets/js/admin-controller.js
+++ client/assets/js/admin-controller.js
@@ -24,6 +24,19 @@ angular.module('portal').controller('adminController', function ($scope, $http, 
         $location.path('/portal'); // not admin, redirect back to root
     });
 
+    var dateFromObjectId = function (objectId) {
+    	return new Date(parseInt(objectId.substring(0, 8), 16) * 1000);
+    };
+
+    var prettyDate = function (date) {
+      var secs = Math.floor((Date.now() - date.getTime()) / 1000);
+      if (secs < 60) return secs + " sec ago";
+      if (secs < 3600) return Math.floor(secs / 60) + " min ago";
+      if (secs < 86400) return Math.floor(secs / 3600) + " hour ago";
+      if (secs < 604800) return Math.floor(secs / 86400) + " day ago";
+      return date.toDateString();
+    }
+
     // adds pretty display fields to project object
     var addDisplayFields = function (project) {
 
@@ -34,6 +47,9 @@ angular.module('portal').controller('adminController', function ($scope, $http, 
         })
         teamDisplay = teamDisplay.substring(0, teamDisplay.length - 2);
         project.teamDisplay = teamDisplay;
+
+        // date
+        project.date = prettyDate(dateFromObjectId(project._id));
 
         return project;
     }
