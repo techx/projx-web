e9d6664ffe040a6c6543f48712bcc25fd15cc9db
Zack Holbrook <zackh@mit.edu>
2018-04-03 23:04:09 -0400

    fix apply

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,6 @@ router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../../client/views/splash.html'));
 });
 
-router.get('/apply', function(req, res, next) {
-    res.redirect('https://goo.gl/forms/pLRx91dFLuoOOywM2');
-});
-
 router.get('/*', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../../client/views/index.html'));
 });
