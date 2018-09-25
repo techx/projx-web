30c63b8896f2e4ca58dc99eb78539cf1f126f3ae
Anurag Golla <agolla07@gmail.com>
2018-09-24 22:17:14 -0400

    initiated server checkpoint to update user model on resume upload success

--- server/routes/user.js
+++ server/routes/user.js
@@ -180,5 +180,30 @@ router.get('/getAdmin', perm.admin, function(req, res) {
 });
 
 
+/**
+ * POST /resumeUpload - [user] Update a user's resume field
+ * @param req.body.email
+ * @param req.body.resume_url
+ * @param req.body.secret_key
+ */
+router.post('/resumeUpload', function(req, res) {
+    User.getUser(req.body.email, function (err, user) {
+        if (err) {
+            res.status(403).send(err);
+        } else {
+            if (req.body.secret_key == config.resume_secret_key) {
+                user.resume = req.body.resume_url
+                User.updateUser(user, function (err, result) {
+                    if (err) res.status(403).send(err);
+                    else res.status(200).send('User updated');
+                });
+            } else {
+                res.status(403).send(err);
+            }
+        }
+    });
+});
+
+
 // EXPORTS //
 module.exports = router;
